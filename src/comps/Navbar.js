b18ba4eb6c6925f92c954f824abe89fa76e6756f
lordmaltazor <malte.svensen@hotmail.com>
2021-03-11 09:56:33 +0100

    added mobile navigation bar

--- src/comps/Navbar.js
+++ src/comps/Navbar.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { auth } from '../FirebaseConfig'
 
 export default function Navbar({ setPage }) {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false); // If the mobile hamburger-menu is currently open
+
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen(!mobileMenuOpen);
+    }
+
     return (
         <div className="navbar">
             <button className="navbar-button navbar-title" onClick={() => setPage(0)}>YuoTube</button>
@@ -9,6 +15,31 @@ export default function Navbar({ setPage }) {
             <button className="navbar-button navbar-home-button" onClick={() => setPage(0)}>Home</button>
             <button className="navbar-button navbar-sign-out-button" onClick={() => auth.signOut()}>Sign out</button>
             <button className="button navbar-upload-button" onClick={() => setPage(2)}>Upload</button>
+
+            <button className="button navbar-mobile-button" onClick={toggleMobileMenu}>
+                <i className="fas fa-bars"></i>
+            </button>
+
+            {mobileMenuOpen && <div className="mobile-menu">
+                <button className="button mobile-menu-button" onClick={() => {
+                    setPage(0);
+                    setMobileMenuOpen(false);
+                }}>Home</button>
+
+                <button className="button mobile-menu-button" onClick={() => {
+                    auth.signOut();
+                    setMobileMenuOpen(false);
+                }}>Sign out</button>
+
+                <button className="button mobile-menu-button" onClick={() => {
+                    setPage(2);
+                    setMobileMenuOpen(false);
+                }}>Upload</button>
+
+                <button className="mobile-menu-close-button" onClick={() => setMobileMenuOpen(false)}>
+                    <i className="fas fa-times"></i>
+                </button>
+            </div>}
         </div>
     )
 }
